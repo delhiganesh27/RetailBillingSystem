393d48f12b985d2e8146fb281a1950c64a21b368
delhiganesh <ramanavijayakumar73061@gmail.com>
2023-08-26 12:18:32 +0530

    adding bill functionality

--- main.py
+++ main.py
@@ -1,30 +1,33 @@
 from tkinter import *
 from tkinter import messagebox
 import random
+import os
 
 
 # functionality part
 bill_num = random.randint(1, 9999)
-
-
+if not os.path.exists('Bills'):
+    os.mkdir('Bills')
+#Total Function
 def total():
-    global soap, faceCream
-    soap = int(bathSoupEntry.get()) * 30
+    global soap, faceCream,faceWash,hairSpray,hairgel,bodyLotion
+    soap = int(bathSoapEntry.get()) * 30
     faceCream = int(faceCreamEntry.get()) * 40
     faceWash = int(faceWashEntry.get()) * 80
     hairSpray = int(hairSprayEntry.get()) * 55
     hairgel = int(hairGelEntry.get()) * 23
     bodyLotion = int(bodyLotionEntry.get()) * 45
 
-    totalCosPrice = soap + faceCream + faceWash + hairSpray + hairgel + bodyLotion
+    totalCosPrice = float(soap + faceCream + faceWash + hairSpray + hairgel + bodyLotion)
     cosmeticPriceEntry.delete(0, END)
     cosmeticPriceEntry.insert(
-        0, str(totalCosPrice) + " Rs"
+        0, f'{totalCosPrice:.2f} Rs'
     )  # 0 indicates index of where to insert
     cosmeticTax = totalCosPrice * 0.15
     cosmeticTaxEntry.delete(0, END)
-    cosmeticTaxEntry.insert(0, str(cosmeticTax) + "Rs")
+    cosmeticTaxEntry.insert(0, f"{cosmeticTax:.2f} Rs")
 
+    global rice,oil,daal,wheat,sugar,tea
     rice = int(riceEntry.get()) * 100
     oil = int(oilEntry.get()) * 180
     daal = int(daalEntry.get()) * 150
@@ -32,29 +35,45 @@ def total():
     sugar = int(sugarEntry.get()) * 200
     tea = int(teaEntry.get()) * 100
 
-    totalGroceryPrice = rice + oil + daal + wheat + sugar + tea
+    totalGroceryPrice = float(rice + oil + daal + wheat + sugar + tea)
     groceryPriceEntry.delete(0, END)
-    groceryPriceEntry.insert(0, str(totalGroceryPrice) + " Rs")
+    groceryPriceEntry.insert(0, f'{totalGroceryPrice:.2f} Rs')
     groceryTax = totalGroceryPrice * 0.05
     groceryTaxEntry.delete(0, END)
-    groceryTaxEntry.insert(0, str(groceryTax) + "Rs")
+    groceryTaxEntry.insert(0, f'{groceryTax:.2f} Rs')
 
+    global maaza,pepsi,sprite,dew,frooti,coke,totalBill
     maaza = int(maazaEntry.get()) * 50
     pepsi = int(pepsiEntry.get()) * 50
     sprite = int(spriteEntry.get()) * 50
     dew = int(dewEntry.get()) * 50
     frooti = int(frootiEntry.get()) * 50
-    coca = int(cocaColaEntry.get()) * 50
+    coke = int(cocaColaEntry.get()) * 50
 
-    totalDrinkPrice = maaza + pepsi + sprite + dew + frooti + coca
+    totalDrinkPrice = float(maaza + pepsi + sprite + dew + frooti + coke)
     coolDrinkPriceEntry.delete(0, END)
-    coolDrinkPriceEntry.insert(0, str(totalDrinkPrice) + " Rs")
+    coolDrinkPriceEntry.insert(0, f'{totalDrinkPrice:.2f} Rs')
     coolDrinkTax = totalDrinkPrice * 0.1
     coolDrinkTaxEntry.delete(0, END)
-    coolDrinkTaxEntry.insert(0, str(coolDrinkTax) + "Rs")
+    coolDrinkTaxEntry.insert(0, f'{coolDrinkTax:.2f} Rs')
 
+    totalBill=totalCosPrice+totalDrinkPrice+totalGroceryPrice+cosmeticTax+groceryTax+coolDrinkTax
 
+#save function
+def saveBill():
+    global bill_num
+    result=messagebox.askyesno('Confirm',"Do you want to save the bill?")
+    if result:
+        billContent=textArea.get(1.0,END)
+        billFile=open(f'Bills/{bill_num}.txt','w')
+        billFile.write(billContent)
+        billFile.close()
+        messagebox.showinfo("Success",f"Bill No:{bill_num} is saved successfully!!!")
+        bill_num=random.randint(1,9999)
+
+#Bill function
 def bill():
+    textArea.delete(1.0,END)
     if nameEntry.get() == "" or phoneEntry.get() == "":
         messagebox.showerror("Error", "Customer details are missing!!!")
     elif (
@@ -73,10 +92,83 @@ def bill():
         textArea.insert(END, "\t\t*Welcome Customer*")
         textArea.insert(END, f"\nBill Number:{bill_num}")
         textArea.insert(END, f"\nCustomer Name:{nameEntry.get()}")
-        textArea.insert(END, f"\nPhone number:+91 {phoneEntry.get()}")
-        textArea.insert(END, "\n=====================================")
-        textArea.insert(END, "\nProduct\t\tQuality\t\tPrice")
-        textArea.insert(END, "\n=====================================")
+        textArea.insert(END, f"\nPhone number:+91 {phoneEntry.get()}\n")
+        textArea.insert(END,"="*63)
+        textArea.insert(END, "\n Product\t\t\tQuality\t\t\tPrice\n")
+        textArea.insert(END,"="*63)
+        if bathSoapEntry.get()!='0':
+            textArea.insert(END,f'\n BathSoap\t\t\t{bathSoapEntry.get()}\t\t\t{soap} Rs')
+        
+        if faceCreamEntry.get()!='0':
+            textArea.insert(END,f'\n Face Cream\t\t\t{faceCreamEntry.get()}\t\t\t{faceCream} Rs')
+        
+        if faceWashEntry.get()!='0':
+            textArea.insert(END,f'\n Face Wash\t\t\t{faceWashEntry.get()}\t\t\t{faceWash} Rs')
+            
+        if hairSprayEntry.get()!='0':
+            textArea.insert(END,f'\n Hair Spray\t\t\t{hairSprayEntry.get()}\t\t\t{hairSpray} Rs')
+            
+        if hairGelEntry.get()!='0':
+            textArea.insert(END,f'\n Hair Gel\t\t\t{hairGelEntry.get()}\t\t\t{hairgel} Rs')
+                
+        if bodyLotionEntry.get()!='0':
+            textArea.insert(END,f'\n Body Lotion\t\t\t{bodyLotionEntry.get()}\t\t\t{bodyLotion} Rs')
+                
+        if riceEntry.get()!='0':
+            textArea.insert(END,f'\n Rice\t\t\t{riceEntry.get()}\t\t\t{rice} Rs')
+                
+        if oilEntry.get()!='0':
+            textArea.insert(END,f'\n Oil\t\t\t{oilEntry.get()}\t\t\t{oil} Rs')
+                
+        if daalEntry.get()!='0':
+            textArea.insert(END,f'\n Daal\t\t\t{daalEntry.get()}\t\t\t{daal} Rs')
+                
+        if wheatEntry.get()!='0':
+            textArea.insert(END,f'\n Wheat\t\t\t{wheatEntry.get()}\t\t\t{wheat} Rs')
+                
+        if sugarEntry.get()!='0':
+            textArea.insert(END,f'\n Sugar\t\t\t{sugarEntry.get()}\t\t\t{sugar} Rs')
+                
+        if teaEntry.get()!='0':
+            textArea.insert(END,f'\n BathSoap\t\t\t{teaEntry.get()}\t\t\t{tea} Rs')
+                
+                
+        if maazaEntry.get()!='0':
+            textArea.insert(END,f'\n BathSoap\t\t\t{maazaEntry.get()}\t\t\t{maaza} Rs')
+            
+        if pepsiEntry.get()!='0':
+            textArea.insert(END,f'\n Pepsi\t\t\t{pepsiEntry.get()}\t\t\t{pepsi} Rs')
+                
+        if spriteEntry.get()!='0':
+            textArea.insert(END,f'\n Sprite\t\t\t{spriteEntry.get()}\t\t\t{sprite} Rs')
+            
+        if dewEntry.get()!='0':
+            textArea.insert(END,f'\n Dew\t\t\t{dewEntry.get()}\t\t\t{dew} Rs')
+        
+        if frootiEntry.get()!='0':
+            textArea.insert(END,f'\n Frooti\t\t\t{frootiEntry.get()}\t\t\t{frooti} Rs')
+            
+        if cocaColaEntry.get()!='0':
+            textArea.insert(END,f'\n Coca Cola\t\t\t{cocaColaEntry.get()}\t\t\t{coke} Rs')
+
+        textArea.insert(END,"\n"+"-"*63)
+        
+        if cosmeticTaxEntry.get()!='0.0 Rs':
+            textArea.insert(END,f'\n Cosmetic Tax\t\t\t\t\t\t{cosmeticTaxEntry.get()}')
+            
+        if groceryTaxEntry.get()!='0.0 Rs':
+            textArea.insert(END,f'\n Grocery Tax\t\t\t\t\t\t{groceryTaxEntry.get()}')
+            
+        if coolDrinkTaxEntry.get()!='0.0 Rs':
+            textArea.insert(END,f"\n Cool Drink Tax\t\t\t\t\t\t{coolDrinkTaxEntry.get()}")
+            
+        textArea.insert(END,f'\n\n Total\t\t\t\t\t\t{totalBill} Rs\n')
+        
+        textArea.insert(END, "-"*63)
+        
+        saveBill()
+        
+        
 
 
 # GUI part
@@ -177,9 +269,9 @@ bathSoupLabel = Label(
 )
 bathSoupLabel.grid(sticky="w", row=0, column=0, padx=8, pady=5)
 
-bathSoupEntry = Entry(cosmeticsFrame, font=("arial", 15), bd=7, width=10)
-bathSoupEntry.grid(row=0, column=1, padx=10, pady=5, sticky="w")
-bathSoupEntry.insert(
+bathSoapEntry = Entry(cosmeticsFrame, font=("arial", 15), bd=7, width=10)
+bathSoapEntry.grid(row=0, column=1, padx=10, pady=5, sticky="w")
+bathSoapEntry.insert(
     0, 0
 )  # to avoid string literal error when a null is converted into int
 
@@ -540,7 +632,7 @@ coolDrinkTaxEntry.grid(row=2, column=3, padx=8, pady=5)
 
 # button frame
 buttonFrame = Frame(billmenuFrame, bd=8, relief=GROOVE)
-buttonFrame.grid(row=0, column=4, rowspan=3, padx=30)
+buttonFrame.grid(row=0, column=4, rowspan=3, padx=50)
 
 totalBtn = Button(
     buttonFrame,
@@ -598,7 +690,7 @@ printBtn.grid(row=0, column=3, pady=20, padx=5)
 
 clearBtn = Button(
     buttonFrame,
-    text="Print",
+    text="Clear",
     font=("arial", 16, "bold"),
     bg="gray20",
     fg="white",
