c3c3e2b7db8a9c761155176689805c157f8b6989
delhiganesh <ramanavijayakumar73061@gmail.com>
2023-08-26 20:57:19 +0530

    adding print and email functionality

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 from tkinter import *
 from tkinter import messagebox
 import random
-import os
+import os, tempfile
+
+# tempfile is an inbuild module to create a temprory files bcs print fun wants a temp file
 
 
 # functionality part
 bill_num = random.randint(1, 9999)
-if not os.path.exists('Bills'):
-    os.mkdir('Bills')
-#Total Function
+if not os.path.exists("Bills"):
+    os.mkdir("Bills")
+
+
+# Total Function
 def total():
-    global soap, faceCream,faceWash,hairSpray,hairgel,bodyLotion
+    global soap, faceCream, faceWash, hairSpray, hairgel, bodyLotion
     soap = int(bathSoapEntry.get()) * 30
     faceCream = int(faceCreamEntry.get()) * 40
     faceWash = int(faceWashEntry.get()) * 80
@@ -18,16 +22,17 @@ def total():
     hairgel = int(hairGelEntry.get()) * 23
     bodyLotion = int(bodyLotionEntry.get()) * 45
 
-    totalCosPrice = float(soap + faceCream + faceWash + hairSpray + hairgel + bodyLotion)
+    totalCosPrice = float(
+        soap + faceCream + faceWash + hairSpray + hairgel + bodyLotion
+    )
     cosmeticPriceEntry.delete(0, END)
-    cosmeticPriceEntry.insert(
-        0, f'{totalCosPrice:.2f} Rs'
-    )  # 0 indicates index of where to insert
+    cosmeticPriceEntry.insert(0, f"{totalCosPrice:.2f} Rs")
+    # 0 indicates index of where to insert
     cosmeticTax = totalCosPrice * 0.15
     cosmeticTaxEntry.delete(0, END)
     cosmeticTaxEntry.insert(0, f"{cosmeticTax:.2f} Rs")
 
-    global rice,oil,daal,wheat,sugar,tea
+    global rice, oil, daal, wheat, sugar, tea
     rice = int(riceEntry.get()) * 100
     oil = int(oilEntry.get()) * 180
     daal = int(daalEntry.get()) * 150
@@ -37,12 +42,12 @@ def total():
 
     totalGroceryPrice = float(rice + oil + daal + wheat + sugar + tea)
     groceryPriceEntry.delete(0, END)
-    groceryPriceEntry.insert(0, f'{totalGroceryPrice:.2f} Rs')
+    groceryPriceEntry.insert(0, f"{totalGroceryPrice:.2f} Rs")
     groceryTax = totalGroceryPrice * 0.05
     groceryTaxEntry.delete(0, END)
-    groceryTaxEntry.insert(0, f'{groceryTax:.2f} Rs')
+    groceryTaxEntry.insert(0, f"{groceryTax:.2f} Rs")
 
-    global maaza,pepsi,sprite,dew,frooti,coke,totalBill
+    global maaza, pepsi, sprite, dew, frooti, coke, totalBill
     maaza = int(maazaEntry.get()) * 50
     pepsi = int(pepsiEntry.get()) * 50
     sprite = int(spriteEntry.get()) * 50
@@ -52,28 +57,37 @@ def total():
 
     totalDrinkPrice = float(maaza + pepsi + sprite + dew + frooti + coke)
     coolDrinkPriceEntry.delete(0, END)
-    coolDrinkPriceEntry.insert(0, f'{totalDrinkPrice:.2f} Rs')
+    coolDrinkPriceEntry.insert(0, f"{totalDrinkPrice:.2f} Rs")
     coolDrinkTax = totalDrinkPrice * 0.1
     coolDrinkTaxEntry.delete(0, END)
-    coolDrinkTaxEntry.insert(0, f'{coolDrinkTax:.2f} Rs')
+    coolDrinkTaxEntry.insert(0, f"{coolDrinkTax:.2f} Rs")
 
-    totalBill=totalCosPrice+totalDrinkPrice+totalGroceryPrice+cosmeticTax+groceryTax+coolDrinkTax
+    totalBill = (
+        totalCosPrice
+        + totalDrinkPrice
+        + totalGroceryPrice
+        + cosmeticTax
+        + groceryTax
+        + coolDrinkTax
+    )
 
-#save function
+
+# save function
 def saveBill():
     global bill_num
-    result=messagebox.askyesno('Confirm',"Do you want to save the bill?")
+    result = messagebox.askyesno("Confirm", "Do you want to save the bill?")
     if result:
-        billContent=textArea.get(1.0,END)
-        billFile=open(f'Bills/{bill_num}.txt','w')
+        billContent = textArea.get(1.0, END)
+        billFile = open(f"Bills/{bill_num}.txt", "w")
         billFile.write(billContent)
         billFile.close()
-        messagebox.showinfo("Success",f"Bill No:{bill_num} is saved successfully!!!")
-        bill_num=random.randint(1,9999)
+        messagebox.showinfo("Success", f"Bill No:{bill_num} is saved successfully!!!")
+        bill_num = random.randint(1, 9999)
 
-#Bill function
+
+# Bill function
 def bill():
-    textArea.delete(1.0,END)
+    textArea.delete(1.0, END)
     if nameEntry.get() == "" or phoneEntry.get() == "":
         messagebox.showerror("Error", "Customer details are missing!!!")
     elif (
@@ -93,82 +107,233 @@ def bill():
         textArea.insert(END, f"\nBill Number:{bill_num}")
         textArea.insert(END, f"\nCustomer Name:{nameEntry.get()}")
         textArea.insert(END, f"\nPhone number:+91 {phoneEntry.get()}\n")
-        textArea.insert(END,"="*63)
+        textArea.insert(END, "=" * 63)
         textArea.insert(END, "\n Product\t\t\tQuality\t\t\tPrice\n")
-        textArea.insert(END,"="*63)
-        if bathSoapEntry.get()!='0':
-            textArea.insert(END,f'\n BathSoap\t\t\t{bathSoapEntry.get()}\t\t\t{soap} Rs')
-        
-        if faceCreamEntry.get()!='0':
-            textArea.insert(END,f'\n Face Cream\t\t\t{faceCreamEntry.get()}\t\t\t{faceCream} Rs')
-        
-        if faceWashEntry.get()!='0':
-            textArea.insert(END,f'\n Face Wash\t\t\t{faceWashEntry.get()}\t\t\t{faceWash} Rs')
-            
-        if hairSprayEntry.get()!='0':
-            textArea.insert(END,f'\n Hair Spray\t\t\t{hairSprayEntry.get()}\t\t\t{hairSpray} Rs')
-            
-        if hairGelEntry.get()!='0':
-            textArea.insert(END,f'\n Hair Gel\t\t\t{hairGelEntry.get()}\t\t\t{hairgel} Rs')
-                
-        if bodyLotionEntry.get()!='0':
-            textArea.insert(END,f'\n Body Lotion\t\t\t{bodyLotionEntry.get()}\t\t\t{bodyLotion} Rs')
-                
-        if riceEntry.get()!='0':
-            textArea.insert(END,f'\n Rice\t\t\t{riceEntry.get()}\t\t\t{rice} Rs')
-                
-        if oilEntry.get()!='0':
-            textArea.insert(END,f'\n Oil\t\t\t{oilEntry.get()}\t\t\t{oil} Rs')
-                
-        if daalEntry.get()!='0':
-            textArea.insert(END,f'\n Daal\t\t\t{daalEntry.get()}\t\t\t{daal} Rs')
-                
-        if wheatEntry.get()!='0':
-            textArea.insert(END,f'\n Wheat\t\t\t{wheatEntry.get()}\t\t\t{wheat} Rs')
-                
-        if sugarEntry.get()!='0':
-            textArea.insert(END,f'\n Sugar\t\t\t{sugarEntry.get()}\t\t\t{sugar} Rs')
-                
-        if teaEntry.get()!='0':
-            textArea.insert(END,f'\n BathSoap\t\t\t{teaEntry.get()}\t\t\t{tea} Rs')
-                
-                
-        if maazaEntry.get()!='0':
-            textArea.insert(END,f'\n BathSoap\t\t\t{maazaEntry.get()}\t\t\t{maaza} Rs')
-            
-        if pepsiEntry.get()!='0':
-            textArea.insert(END,f'\n Pepsi\t\t\t{pepsiEntry.get()}\t\t\t{pepsi} Rs')
-                
-        if spriteEntry.get()!='0':
-            textArea.insert(END,f'\n Sprite\t\t\t{spriteEntry.get()}\t\t\t{sprite} Rs')
-            
-        if dewEntry.get()!='0':
-            textArea.insert(END,f'\n Dew\t\t\t{dewEntry.get()}\t\t\t{dew} Rs')
-        
-        if frootiEntry.get()!='0':
-            textArea.insert(END,f'\n Frooti\t\t\t{frootiEntry.get()}\t\t\t{frooti} Rs')
-            
-        if cocaColaEntry.get()!='0':
-            textArea.insert(END,f'\n Coca Cola\t\t\t{cocaColaEntry.get()}\t\t\t{coke} Rs')
+        textArea.insert(END, "=" * 63)
+        if bathSoapEntry.get() != "0":
+            textArea.insert(
+                END, f"\n BathSoap\t\t\t{bathSoapEntry.get()}\t\t\t{soap} Rs"
+            )
 
-        textArea.insert(END,"\n"+"-"*63)
-        
-        if cosmeticTaxEntry.get()!='0.0 Rs':
-            textArea.insert(END,f'\n Cosmetic Tax\t\t\t\t\t\t{cosmeticTaxEntry.get()}')
-            
-        if groceryTaxEntry.get()!='0.0 Rs':
-            textArea.insert(END,f'\n Grocery Tax\t\t\t\t\t\t{groceryTaxEntry.get()}')
-            
-        if coolDrinkTaxEntry.get()!='0.0 Rs':
-            textArea.insert(END,f"\n Cool Drink Tax\t\t\t\t\t\t{coolDrinkTaxEntry.get()}")
-            
-        textArea.insert(END,f'\n\n Total\t\t\t\t\t\t{totalBill} Rs\n')
-        
-        textArea.insert(END, "-"*63)
-        
+        if faceCreamEntry.get() != "0":
+            textArea.insert(
+                END, f"\n Face Cream\t\t\t{faceCreamEntry.get()}\t\t\t{faceCream} Rs"
+            )
+
+        if faceWashEntry.get() != "0":
+            textArea.insert(
+                END, f"\n Face Wash\t\t\t{faceWashEntry.get()}\t\t\t{faceWash} Rs"
+            )
+
+        if hairSprayEntry.get() != "0":
+            textArea.insert(
+                END, f"\n Hair Spray\t\t\t{hairSprayEntry.get()}\t\t\t{hairSpray} Rs"
+            )
+
+        if hairGelEntry.get() != "0":
+            textArea.insert(
+                END, f"\n Hair Gel\t\t\t{hairGelEntry.get()}\t\t\t{hairgel} Rs"
+            )
+
+        if bodyLotionEntry.get() != "0":
+            textArea.insert(
+                END, f"\n Body Lotion\t\t\t{bodyLotionEntry.get()}\t\t\t{bodyLotion} Rs"
+            )
+
+        if riceEntry.get() != "0":
+            textArea.insert(END, f"\n Rice\t\t\t{riceEntry.get()}\t\t\t{rice} Rs")
+
+        if oilEntry.get() != "0":
+            textArea.insert(END, f"\n Oil\t\t\t{oilEntry.get()}\t\t\t{oil} Rs")
+
+        if daalEntry.get() != "0":
+            textArea.insert(END, f"\n Daal\t\t\t{daalEntry.get()}\t\t\t{daal} Rs")
+
+        if wheatEntry.get() != "0":
+            textArea.insert(END, f"\n Wheat\t\t\t{wheatEntry.get()}\t\t\t{wheat} Rs")
+
+        if sugarEntry.get() != "0":
+            textArea.insert(END, f"\n Sugar\t\t\t{sugarEntry.get()}\t\t\t{sugar} Rs")
+
+        if teaEntry.get() != "0":
+            textArea.insert(END, f"\n BathSoap\t\t\t{teaEntry.get()}\t\t\t{tea} Rs")
+
+        if maazaEntry.get() != "0":
+            textArea.insert(END, f"\n BathSoap\t\t\t{maazaEntry.get()}\t\t\t{maaza} Rs")
+
+        if pepsiEntry.get() != "0":
+            textArea.insert(END, f"\n Pepsi\t\t\t{pepsiEntry.get()}\t\t\t{pepsi} Rs")
+
+        if spriteEntry.get() != "0":
+            textArea.insert(END, f"\n Sprite\t\t\t{spriteEntry.get()}\t\t\t{sprite} Rs")
+
+        if dewEntry.get() != "0":
+            textArea.insert(END, f"\n Dew\t\t\t{dewEntry.get()}\t\t\t{dew} Rs")
+
+        if frootiEntry.get() != "0":
+            textArea.insert(END, f"\n Frooti\t\t\t{frootiEntry.get()}\t\t\t{frooti} Rs")
+
+        if cocaColaEntry.get() != "0":
+            textArea.insert(
+                END, f"\n Coca Cola\t\t\t{cocaColaEntry.get()}\t\t\t{coke} Rs"
+            )
+
+        textArea.insert(END, "\n" + "-" * 63)
+
+        if cosmeticTaxEntry.get() != "0.0 Rs":
+            textArea.insert(END, f"\n Cosmetic Tax\t\t\t\t\t\t{cosmeticTaxEntry.get()}")
+
+        if groceryTaxEntry.get() != "0.0 Rs":
+            textArea.insert(END, f"\n Grocery Tax\t\t\t\t\t\t{groceryTaxEntry.get()}")
+
+        if coolDrinkTaxEntry.get() != "0.0 Rs":
+            textArea.insert(
+                END, f"\n Cool Drink Tax\t\t\t\t\t\t{coolDrinkTaxEntry.get()}"
+            )
+
+        textArea.insert(END, f"\n\n Total\t\t\t\t\t\t{totalBill} Rs\n")
+
+        textArea.insert(END, "-" * 63)
+
         saveBill()
-        
-        
+
+    # search function
+
+
+def searchBill():
+    for i in os.listdir("Bills/"):
+        if i.split(".")[0] == billNumEntry.get():
+            file = open(f"Bills/{i}", "r")
+            textArea.delete(1.0, END)
+            for data in file:
+                textArea.insert(END, data)
+            file.close()
+            break
+    else:
+        messagebox.showerror("Error", "Invalid bill number")
+
+
+def printBill():
+    if textArea.get(1.0, END) == "\n":  # if there is no content text area is not null
+        messagebox.showerror("Error", "Bill is empty")
+    else:
+        file = tempfile.mktemp(".txt")
+        open(file, "w").write(textArea.get(1.0, END))
+        os.startfile(file, "print")
+        # startfile takes 2 args file and command like print
+
+
+def send_email():
+    if textArea.get(1.0, END) == "\n":  # if there is no content text area is not null
+        messagebox.showerror("Error", "Bill is empty")
+    else:
+        branch1 = Toplevel()  # we may also use Tk()
+        branch1.title("Send Email")
+        branch1.config(bg="gray20")
+        branch1.resizable(0, 0)  # disable the maximization
+
+        senderFrame = LabelFrame(
+            branch1,
+            text="SENDER",
+            font=("arial", 16, "bold"),
+            bd=6,
+            bg="gray20",
+            fg="white",
+        )
+        senderFrame.grid(row=0, column=0, padx=40, pady=20)
+
+        senderLabel = Label(
+            senderFrame,
+            text="Sender's Email",
+            font=("arial", 14, "bold"),
+            bd=6,
+            bg="gray20",
+            fg="white",
+        )
+        senderLabel.grid(row=0, column=0, sticky="w")
+
+        senderEntry = Entry(
+            senderFrame,
+            font=("arial", 14, "bold"),
+            bd=6,
+            width=23,
+            relief=RIDGE,
+        )
+        senderEntry.grid(row=0, column=1, padx=10, pady=8)
+
+        passwordLabel = Label(
+            senderFrame,
+            text="Password",
+            font=("arial", 14, "bold"),
+            bd=6,
+            bg="gray20",
+            fg="white",
+        )
+        passwordLabel.grid(row=1, column=0, sticky="w")
+
+        passwordEntry = Entry(
+            senderFrame,
+            font=("arial", 14, "bold"),
+            bd=6,
+            width=23,
+            relief=RIDGE,
+        )
+        passwordEntry.grid(row=1, column=1, padx=10, pady=8)
+
+        receiverFrame = LabelFrame(
+            branch1,
+            text="Recipient",
+            font=("arial", 16, "bold"),
+            bd=6,
+            bg="gray20",
+            fg="white",
+        )
+        receiverFrame.grid(row=1, column=0, padx=40, pady=20)
+
+        receiverLabel = Label(
+            receiverFrame,
+            text="Email Address",
+            font=("arial", 14, "bold"),
+            bd=6,
+            bg="gray20",
+            fg="white",
+        )
+        receiverLabel.grid(row=0, column=0, sticky="w")
+
+        receiverEntry = Entry(
+            receiverFrame,
+            font=("arial", 14, "bold"),
+            bd=6,
+            width=23,
+            relief=RIDGE,
+        )
+        receiverEntry.grid(row=0, column=1, padx=10, pady=8)
+
+        messageLabel = Label(
+            receiverFrame,
+            text="Message",
+            font=("arial", 14, "bold"),
+            bd=6,
+            bg="gray20",
+            fg="white",
+        )
+        messageLabel.grid(row=1, column=0, sticky="w")
+
+        emailTextArea = Text(
+            receiverFrame,
+            font=("arial", 14, "bold"),
+            bd=2,
+            relief=SUNKEN,
+            width=42,
+            height=11,
+        )
+        emailTextArea.grid(row=2, column=0, columnspan=2)
+
+        sendBtn = Button(
+            branch1, text="SEND", font=("arial", 16, "bold"), width=15, bg="forestgreen"
+        )
+        sendBtn.grid(row=2, column=0, pady=20)
+        branch1.mainloop()
 
 
 # GUI part
@@ -240,7 +405,12 @@ billNumEntry = Entry(customDeFrame, font=("arial", 15), bd=7, width=18)
 billNumEntry.grid(row=0, column=5, padx=8)
 
 searchButton = Button(
-    customDeFrame, text="SEARCH", font=("arial", 12, "bold"), bd=7, width=10
+    customDeFrame,
+    text="SEARCH",
+    font=("arial", 12, "bold"),
+    bd=7,
+    width=10,
+    command=searchBill,
 )
 searchButton.grid(row=0, column=6, padx=20, pady=8)
 
@@ -673,6 +843,7 @@ emailBtn = Button(
     bd=5,
     width=8,
     pady=10,
+    command=send_email,
 )  # when pad is mentioned here instead of grid it is internal pading
 emailBtn.grid(row=0, column=2, pady=20, padx=5)
 
@@ -684,6 +855,7 @@ printBtn = Button(
     fg="white",
     bd=5,
     width=8,
+    command=printBill,
     pady=10,
 )  # when pad is mentioned here instead of grid it is internal pading
 printBtn.grid(row=0, column=3, pady=20, padx=5)
